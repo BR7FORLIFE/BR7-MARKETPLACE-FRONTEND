1115970c56d476012616f69156ecf9286290040a
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-07 13:50:02 -0500

    feat: implement App component with routing and provider setup

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,7 +1,11 @@
+import AppRouter from "./routes/router.tsx"
+import AppProvider from './routes/provider.tsx'
 function App() {
   
   return (
-    <></>
+    <AppProvider>
+      <AppRouter/>
+    </AppProvider>
   )
 }
 
