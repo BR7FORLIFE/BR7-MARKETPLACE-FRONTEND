696c13b64a6426fb79c121adf1dc5ee167d884fb
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-07 13:49:38 -0500

    feat: update ESLint and package configurations, add TanStack Query support

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@config": "/src/config",
+            "@routes": "/src/routes",
+        },
+    },
 });
