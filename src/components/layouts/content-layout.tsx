74654d844807fc631be0f25c2122f8fd92ee480d
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-12 12:40:06 -0500

    feat: struct the content layout and insert header and footer

---
+++ src/components/layouts/content-layout.tsx
@@ -0,0 +1,23 @@
+import Footer from '../UI/global/footer';
+import { Header } from '../UI/global/header-nav'
+
+interface Props{
+    title: string,
+    children: React.ReactNode
+}
+
+function ContentLayout({title, children}: Props){
+
+    return (
+        <section className='w-full h-full flex flex-col justify-between items-center'>
+            <Header/>
+            <main className='w-full h-full'>
+                {children}
+            </main>
+            <Footer/>
+        </section>
+    )
+
+}
+
+export default ContentLayout
