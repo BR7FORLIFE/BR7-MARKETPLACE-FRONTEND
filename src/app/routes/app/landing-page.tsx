7d605c5d077cdfb2e02a0e69298d4e5b0da476e3
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-12 12:38:54 -0500

    feat: started to create a lading page

---
+++ src/app/routes/app/landing-page.tsx
@@ -0,0 +1,13 @@
+import ContentLayout from "../../../components/layouts/content-layout"
+
+function LandingRoute(){
+    return (
+        <ContentLayout title="Home">
+            <section className="">
+                
+            </section>            
+        </ContentLayout>
+    )
+}
+
+export default LandingRoute
