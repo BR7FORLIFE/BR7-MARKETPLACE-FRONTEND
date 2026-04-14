479132039e627d4f5dd85e3362c529d93efb1378
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-14 16:01:12 -0500

    refactor: improve code formatting and consistency in router.tsx

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,36 +1,38 @@
-import {createBrowserRouter, RouterProvider} from 'react-router'
-import { ROUTES } from '../../config/path'
-import { useQueryClient, type QueryClient } from '@tanstack/react-query';
-import { useMemo } from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { ROUTES } from "@config/path";
+import { useQueryClient, type QueryClient } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 const convert = (queryClient: QueryClient) => (m: any) => {
-    const {default: Component, clientLoader, clientAction,...rest} = m;
+  const { default: Component, clientLoader, clientAction, ...rest } = m;
 
-    return {
-        ...rest,
-        Component,
-        loader: clientLoader?.(queryClient),
-        action: clientAction?.(queryClient)
-    }
-}
+  return {
+    ...rest,
+    Component,
+    loader: clientLoader?.(queryClient),
+    action: clientAction?.(queryClient),
+  };
+};
 
-export const createAppRoutes = (queryClient: QueryClient) => createBrowserRouter([
+export const createAppRoutes = (queryClient: QueryClient) =>
+  createBrowserRouter([
     {
-        path: ROUTES.home.path,
-        lazy: () => import('./app/landing-page').then(convert(queryClient))
-    }
-])
-
-
-
+      path: ROUTES.home.path,
+      lazy: () => import("./app/landing-page").then(convert(queryClient)),
+    },
+    {
+      path: ROUTES.auth.register.path,
+      lazy: () => import("./auth/register-page").then(convert(queryClient)),
+    },
+  ]);
 
 //este componente tendra acceso a todos los path y componentes a reutilizar
-function AppRouter(){
-    const queryClient = useQueryClient()
-    
-    const router = useMemo(() => createAppRoutes(queryClient), [queryClient])
+function AppRouter() {
+  const queryClient = useQueryClient();
 
-    return <RouterProvider router={router} />
+  const router = useMemo(() => createAppRoutes(queryClient), [queryClient]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default AppRouter 
+export default AppRouter;
