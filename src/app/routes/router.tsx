ee5fc2a5da0b0b4ad71113cb6a74f69b00447163
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-04-23 11:13:01 -0500

    feat: add lazy loading for login page route in the router configuration

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -24,6 +24,10 @@ export const createAppRoutes = (queryClient: QueryClient) =>
       path: ROUTES.auth.register.path,
       lazy: () => import("./auth/register-page").then(convert(queryClient)),
     },
+    {
+      path: ROUTES.auth.login.path,
+      lazy: () => import("./auth/login-page").then(convert(queryClient))
+    }
   ]);
 
 //este componente tendra acceso a todos los path y componentes a reutilizar
