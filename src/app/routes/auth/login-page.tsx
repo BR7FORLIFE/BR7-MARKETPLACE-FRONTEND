489435cab3ac276a4fb76f27a1852cfa5282e96e
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-24 15:59:35 -0500

    feat: implement LoginRoute and enhance RegisterRoute with verification state and JoinCard

---
+++ src/app/routes/auth/login-page.tsx
@@ -0,0 +1,9 @@
+import AuthLayout from '@components/layouts/auth-layout'
+
+function LoginRoute(){
+    <AuthLayout title='login'>
+        <div>Hola</div>
+    </AuthLayout>
+}
+
+export default LoginRoute
--- src/app/routes/auth/register-page.tsx
+++ src/app/routes/auth/register-page.tsx
@@ -1,17 +1,28 @@
-import AuthLayout from '@components/layouts/auth-layout'
-import RegisterForm from '@features/auth/components/RegisterForm'
+import AuthLayout from "@components/layouts/auth-layout";
+import RegisterForm from "@features/auth/components/RegisterForm";
 
-function RegisterRoute(){
-    return (
-        <AuthLayout title='register'>
-            <section className='flex flex-col md:flex-row gap-3'>
-                <div>
-                    
-                </div>
-                <RegisterForm/>
-            </section>
-        </AuthLayout>
-    )
+import { JoinCard } from "../../../components/blocks/PromoCard";
+import { useState } from "react";
+import { VerificationEmailRoute } from "./verification-email";
+
+function RegisterRoute() {
+
+  const [verifyEmail, setVerifyEmail] = useState(false)
+
+  return (
+    <AuthLayout title="register">
+      <section className="flex flex-col md:flex-row gap-3 justify-center w-[80%] h-[90%]">
+        <JoinCard
+          title="THE MONOLITHIC STANDARD."
+          description="Join an ecosystem where architecture eets commerce. BR7-MARKETPLACE is a curated digital archive for those who demand structural integrity in every interaction."
+          styles={{
+            height: "100%",
+          }}
+        />
+        {verifyEmail ? <VerificationEmailRoute/> : <RegisterForm  verifyFn={setVerifyEmail}/> }
+      </section>
+    </AuthLayout>
+  );
 }
 
-export default RegisterRoute
+export default RegisterRoute;
