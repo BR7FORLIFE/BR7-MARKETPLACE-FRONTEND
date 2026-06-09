05811ea25aaa78c362adc3c3cffad5fd2095241a
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-09 11:32:05 -0500

    feat: add request and response interceptors for accessToken management in API calls

--- src/lib/api-endpoints.ts
+++ src/lib/api-endpoints.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../stores/auth-store";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_SPRING_DEFAULT_PATH_LOCAL,
@@ -7,6 +8,42 @@ export const api = axios.create({
     },
     withCredentials: true,
 });
+
+//inyectar el accessToken en la cabecera Authorization
+api.interceptors.request.use((config) => {
+    const token = useAuthStore().getAccessToken();
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+//si en caso tal ha expirado el accessToken hacer el refresh y obtener el nuevo
+api.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401) {
+            const refreshResponse = await refreshToken(); //crear la funcion para hacer el refresh
+
+            const token = refreshResponse.accessToken;
+
+            useAuthStore.setState({
+                accessToken: token,
+            });
+
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+
+            return api(originalRequest);
+        }
+
+        return Promise.reject(error);
+    },
+);
 
 export const API_ENDPOINTS = {
     AUTH: {
